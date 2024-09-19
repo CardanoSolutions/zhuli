17f31207d13d0c48ab35a54e3bb0c67f6e57585f
KtorZ <matthias.benkort@gmail.com>
2024-09-19 18:22:50 +0200

    Setup cargo-dist

--- cli/src/contract.rs
+++ cli/src/contract.rs
@@ -1,3 +1,7 @@
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 use crate::{cardano::Cardano, pallas_extra::*};
 use pallas_addresses::ShelleyAddress;
 use pallas_codec::utils::Bytes;
